edae155d545bfdf5b0aa49126ee53274e174e064
josemagr95 <josegomezrivera95@gmail.com>
2020-06-10 00:28:39 -0400

    js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,18 @@ $('svg').click(function() {
   playing = !playing;
   var animation = playing ? 'stop' : 'play';
   $('#animate_to_' + animation).get(0).beginElement();
+  if (playing == true) {
+    $(".texto").hide();
+    $(".titulo-simulacion").hide();
+    $(".subtitulo-simulacion").hide();
+    $("footer").hide();
+    $(".subtitulo-simulacion").hide();
+    $("#startButton").hide();
+  }
+
+  if (playin == false) {
+    $("#startButton").show();
+  }
   
   $('.texto').text('Click para ' + (playing ? 'Pausar' : 'Iniciar'));
 });
